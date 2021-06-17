e31ee554902b9a60b7811d15c0468f575ab923ad
Marcos Méndez <marcosmh0199@gmail.com>
2021-06-17 07:46:07 -0600

    feat: db credentials adjust

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "ec2-3-212-75-25.compute-1.amazonaws.com",
-  USER: "zavunorhcvlqdl",
-  PASSWORD: "7b69679b668f3e6d9c7f5268dc52e10243a444ef79a486404bc15872019ac56b",
-  DB: "d642g79eq20895",
+  USER: "eccgdbavtjrjvc",
+  PASSWORD: "2e9be62a1003d5e3f31a737f75c7de9b08c17f0a0dac4fb23bcf1f90ca88155d",
+  DB: "d55k37rnpfsui1",
   dialect: "postgres",
   dialectOptions: {
     ssl: {
