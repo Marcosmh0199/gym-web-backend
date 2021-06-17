4279da2043ee5bc2f3df3e4aca1dd26a1c64142a
Marcos Méndez <marcosmh0199@gmail.com>
2021-06-17 08:10:42 -0600

    feat: catalogs

---
+++ src/models/ctEspecialidades.js
@@ -0,0 +1,21 @@
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define('ctEspecialidades', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    nombre: {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    descripcion: {
+      type: DataTypes.STRING(100),
+      allowNull: true
+    }
+  },
+  {
+    tableName: 'ctEspecialidades',
+    timestamps: true
+  });
+};
