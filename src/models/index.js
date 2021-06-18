eb1d4b9bc0101bf547fb128d64ee94456f8b4ecb
Marcos Méndez <marcosmh0199@gmail.com>
2021-06-18 03:54:36 -0600

    feat: index
    contains the tables and relations, also the sequelize ORM objects

---
+++ src/models/index.js
@@ -0,0 +1,70 @@
+
+const dbConfig = require("../config/db.config");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  dialectOptions: dbConfig.dialectOptions,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+//tables
+db.administradores = require('./Administradores')(sequelize, Sequelize);
+db.clases = require('./Clases')(sequelize, Sequelize);
+db.clientes = require('./Clientes')(sequelize, Sequelize);
+db.ctEspecialidades = require('./ctEspecialidades')(sequelize, Sequelize);
+db.ctServicios = require('./ctServicios')(sequelize, Sequelize);
+db.ctTipoPagos = require('./ctTipoPagos')(sequelize, Sequelize);
+db.instructores = require('./Instructores')(sequelize, Sequelize);
+db.pagos = require('./Pagos')(sequelize, Sequelize);
+db.portafolios = require('./Portafolios')(sequelize, Sequelize);
+db.reservaciones = require('./Reservaciones')(sequelize, Sequelize);
+db.salas = require('./Salas')(sequelize, Sequelize);
+db.sesiones = require('./Sesiones')(sequelize, Sequelize);
+
+//relations
+
+//Administradores
+
+//Clases
+db.ctServicios.hasMany(db.clases);
+db.instructores.hasMany(db.clases, { onDelete: 'cascade' });
+
+//Clientes
+
+//ctEspecialidades
+
+//ctServicios
+
+//ctTipoPagos
+
+//Instructores
+db.instructores.belongsTo(db.salas);
+
+//Pagos
+db.pagos.belongsTo(db.reservaciones);
+db.ctTipoPagos.hasMany(db.pagos);
+
+//Portafolios
+db.portafolios.belongsTo(db.instructores);
+db.clases.hasMany(db.portafolios);
+
+//Reservaciones
+db.reservaciones.belongsTo(db.clientes);
+db.sesiones.hasMany(db.reservaciones);
+
+//Salas
+db.administradores.hasMany(db.salas);
+
+//Sesiones
+db.salas.hasMany(db.sesiones);
+
+module.exports = db; 
