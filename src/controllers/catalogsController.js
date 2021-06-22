e1a316a12d4b331039c79f8fcada2482d90ad608
Marcos Méndez <marcosmh0199@gmail.com>
2021-06-22 10:37:47 -0600

    feat: get catalogs

---
+++ src/controllers/catalogsController.js
@@ -0,0 +1,48 @@
+const db = require('../models');
+const { sequelize } = require("../models");
+const { v4: uuidv4 } = require('uuid');
+const Especialidades = db.ctEspecialidades;
+const Servicios = db.ctServicios;
+const TipoPagos = db.ctTipoPagos;
+const CONSTANTS = require('../config/constants');
+const { Op } = require('sequelize');
+const { QueryTypes } = require('sequelize'); 
+
+exports.getEspecialidades = async (req, res, next) => {
+  try {
+    let _especialidades = await Especialidades.findAll();
+    return res.status(200).send({
+      especialidades : _especialidades,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      msg : `Error al recuperar las especialidades: ${error.stack}`,
+    });
+  }
+}
+
+exports.getServicios = async (req, res, next) => {
+  try {
+    let _servicios = await Servicios.findAll();
+    return res.status(200).send({
+      servicios : _servicios,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      msg : `Error al recuperar los servicios: ${error.stack}`,
+    });
+  }
+}
+
+exports.getTipoPagos = async (req, res, next) => {
+  try {
+    let _tipoPagos = await TipoPagos.findAll();
+    return res.status(200).send({
+      TipoPagos : _tipoPagos,
+    });
+  } catch (error) {
+    return res.status(400).send({
+      msg : `Error al recuperar los tipo de pagos: ${error.stack}`,
+    });
+  }
+}
