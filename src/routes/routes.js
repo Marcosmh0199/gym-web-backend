51704698892f917fdcd44661f039b89db2e895ae
Marcos Méndez <marcosmh0199@gmail.com>
2021-06-22 09:22:31 -0600

    feat: clients CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,12 @@
 module.exports = app => {
   var router = require("express").Router();
+
+  const clients = require('../controllers/ClientesController');
+
+  //Clientes
+  router.post('/clientes/create', clients.create);
+  router.post('/clientes/read', clients.get);
+  router.post('/clientes/update', clients.update);
+  router.post('/clientes/delete', clients.delete);
   app.use('', router);
 }
